c8f905d7f62f82691c85065eeb6271cb4b6b3fb1
Ethan Peterson <eap731@byu,edu>
2026-02-19 12:44:43 -0700

    log in funcitonality

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import './login.css';
-import { NavLink } from 'react-router-dom';
+import { useNavigate, NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export function Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = React.useState(localStorage.getItem("email") || "");
+  const [password, setPassword] = React.useState("");
+
+  async function loginUser() {
+    if (localStorage.getItem("email") === email) {
+      if (localStorage.getItem("password") === password) {
+        navigate('/card');
+        return;
+      }
+      else{
+        alert("Incorrect password. Please try again.");
+        return;
+      }
+    }
+    if (email === "" | password === "") {
+      alert("Please fill in all fields.");
+      return;
+    }
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+    navigate('/card');
+  }
+
   return (
     <main className = "text-center">
       <h1 className="mb-4">Create Your Account</h1>
@@ -11,23 +36,21 @@ export function Login() {
       <form onSubmit={(e) => e.preventDefault()} className="mx-auto lognin-form">
 
         <div className="form-floating mb-3">
-          <input type="email" placeholder="✉ Email" className="form-control" id="email"/>
+          <input type="email" placeholder="✉ Email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
           <label htmlFor="email">✉ Email</label>
         </div>
 
         <div className = "form-floating mb-3">
-          <input type="password" placeholder="🗝️ Password" className="form-control" id="password"/>
+          <input type="password" placeholder="🗝️ Password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
           <label htmlFor="password">🗝️ Password</label>
         </div>
 
         <div className="form-check mb-3 text-start ps-0"> 
-            <input className="form-check-impu" type="checkbox" value="" id="remember"/> 
+            <input className="form-check-imput" type="checkbox" value="" id="remember"/> 
             <label className="form-check-label" htmlFor="remember">Remember me</label>
         </div> 
         
-        <NavLink to="/card">
-            <button type="submit" className="btn btn-primary w-100 custom-btn">Sign up / Log in</button>
-        </NavLink>
+        <button type="submit" onClick = {() =>loginUser()} className="btn btn-primary w-100 custom-btn">Sign up / Log in</button>
       </form>
     </main>
   );
