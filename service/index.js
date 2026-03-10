f5cc683f4ac12dc59ecc0c6ddd68bdb6ba0de3c9
Ethan Peterson <eap731@byu,edu>
2026-03-09 19:45:02 -0600

    file organization

--- service/index.js
+++ service/index.js
@@ -1,2 +1,16 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+appluse(cookieParser());
 app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use('/api', apiRouter);
