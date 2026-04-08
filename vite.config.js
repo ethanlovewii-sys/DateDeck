c3b0179ed4646a4b8ef4c4094531f38defef0034
Ethan Peterson <eap731@byu,edu>
2026-04-07 19:34:00 -0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:4000',
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      },
+
+      '/ws': {
+        target: 'ws://localhost:4000',
+        ws: true,
+      },
     },
   },
 });
